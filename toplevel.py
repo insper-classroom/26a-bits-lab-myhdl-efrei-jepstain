a8ec693b51f7162f252b83139064938918d92f98
Heitor-Galindo-Insper <heitorng@al.insper.edu.br>
2026-02-27 18:57:55 +0000

    ex 4

--- toplevel.py
+++ toplevel.py
@@ -10,7 +10,7 @@ def toplevel(LEDR, SW, HEX0, HEX1):
     ledr_s = [Signal(bool(0)) for i in range(10)]
 
     # Instatiations
-    ic1 = exe4(ledr_s, SW)
+    ic1 = exe4(ledr_s, SW) # Feito no ex4
     # ic2 = exe5(ledr_s, SW)
     # ic2 = sw2hex(HEX0, SW)
     # ic3 = bin2hex(HEX1, SW)
@@ -23,16 +23,16 @@ def toplevel(LEDR, SW, HEX0, HEX1):
     return instances()
 
 
-LEDR = Signal(intbv(0)[10:])
-SW = Signal(intbv(0)[10:])
-KEY = Signal(intbv(0)[4:])
-HEX0 = Signal(intbv(1)[7:])
-HEX1 = Signal(intbv(1)[7:])
-HEX2 = Signal(intbv(1)[7:])
-HEX3 = Signal(intbv(1)[7:])
-HEX4 = Signal(intbv(1)[7:])
-HEX5 = Signal(intbv(1)[7:])
-CLOCK_50 = Signal(bool())
+LEDR = Signal(intbv(0)[10:]) # Led RED
+SW = Signal(intbv(0)[10:]) # Switch
+KEY = Signal(intbv(0)[4:]) # Chave
+HEX0 = Signal(intbv(1)[7:]) # Displays 1 a 7
+HEX1 = Signal(intbv(1)[7:]) #
+HEX2 = Signal(intbv(1)[7:]) #
+HEX3 = Signal(intbv(1)[7:]) #
+HEX4 = Signal(intbv(1)[7:]) #
+HEX5 = Signal(intbv(1)[7:]) #
+CLOCK_50 = Signal(bool()) #
 RESET_N = ResetSignal(0, active=0, isasync=True)
 
 top = toplevel(LEDR, SW, HEX0, HEX1)
